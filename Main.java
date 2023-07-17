da140715981ad578716ecf188ffae04384b1e8e8
Elizavveta <137884565+Elizavveta@users.noreply.github.com>
2023-07-18 00:18:02 +0300

    homework#4

--- Main.java
+++ Main.java
@@ -11,4 +11,52 @@ public class Main {
     }
 }
 
+______________________________________________________________
+
+    import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое число: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Введите ыторое число: ");
+        int num2 = scanner.nextInt();
+
+        int sum = addition(num1, num2);
+        System.out.println("Сумма: " + sum);
+
+        int diff = subtraction(num1, num2);
+        System.out.println("Разность: " + diff);
+
+        int product = multiplication(num1, num2);
+        System.out.println("Произведение: " + product);
+
+        double quotient = division(num1, num2);
+        System.out.println("Частное: " + quotient);
+
+    }
+
+    private static double division(int a, int b) {
+        if (b==0)  new Exception("Вы не можете делить на ноль");
+        return a / b;
+    }
+
+
+    private static int multiplication(int a, int b) {
+
+        return a * b;
+    }
+
+    private static int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    private static int addition(int a, int b) {
+        return a + b;
+    }
+
+}
 
